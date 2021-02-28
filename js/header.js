7e53965142d73ca203260d7592d92537f5263c95
sptungG <ngoviettung154@gmail.com>
2021-03-01 00:21:29 +0700

    login form

--- js/header.js
+++ js/header.js
@@ -1,12 +1,26 @@
-$("#signup").click(function () {
-    $(".pinkbox").css("transform", "translateX(80%)");
-    $(".signin").addClass("nodisplay");
-    $(".signup").removeClass("nodisplay");
-  });
-  
-  $("#signin").click(function () {
-    $(".pinkbox").css("transform", "translateX(0%)");
-    $(".signup").addClass("nodisplay");
-    $(".signin").removeClass("nodisplay");
-  });
-  
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("sign-up");
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
