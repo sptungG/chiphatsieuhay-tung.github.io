eba8d0b7ef3049c9556ba5bfb2ce1e63cb00c848
sptungG <ngoviettung154@gmail.com>
2021-03-01 08:28:02 +0700

    update login

--- js/header.js
+++ js/header.js
@@ -1,17 +1,8 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+
 // Get the modal
 var modal = document.getElementById("myModal");
-
 // Get the button that opens the modal
 var btn = document.getElementById("sign-up");
 // When the user clicks the button, open the modal 
